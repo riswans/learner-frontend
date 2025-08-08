9d514d1824ac702a3ae16a26c8a81ff5d40be795
riswanto-com <wansr87@gmail.com>
2025-08-08 17:22:29 +0700

    app

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+    
+</script>
+
+<template>
+    <div class="p-4 bg-amber-500 text-amber-600 flex justify-center border border-amber-300">
+        Halo world
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  ssr: false,
+  app: {
+    head: {
+      title: "Learners",
+      link: [{ rel: "icon", type: "image/png", href: "./logo.png" }],
+    },
+  },
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/icon", "@pinia/nuxt"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
